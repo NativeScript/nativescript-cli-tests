d74be8b11d82963c6e887a1b905331d4de9e39ff
Dimitar Topuzov <dimitar.topuzov@telerik.com>
2015-01-14 09:45:12 +0200

    Update CLI tests

--- tns_tests_osx.py
+++ tns_tests_osx.py
@@ -127,5 +127,5 @@ class TNSTests_OSX(unittest.TestCase):
         command = tnsPath + " device ios"
         output = runAUT(command)     
         
-        assert not ("Error" in output)  
-        assert not ("Android" in output)
+		assert ("iPhone" in output)
+        assert not ("Error" in output)
