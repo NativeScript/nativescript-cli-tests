c65183ea5ae151e3b5ad26e9a90b5cb0c209d9f7
dtopuzov <dimitar.topuzov@telerik.com>
2015-06-11 12:54:03 +0300

    Update iOS debugger tests

--- tests/debug_osx.py
+++ tests/debug_osx.py
@@ -1,9 +1,11 @@
 import unittest
 
-from helpers._os_lib import CleanupFolder
+from helpers._os_lib import CleanupFolder, runAUT
+from helpers._tns_lib import CreateProjectAndAddPlatform, iosRuntimeSymlinkPath, tnsPath
+from helpers.device import GivenRealDeviceRunning
 
 
-class Debug_OSX(unittest.TestCase):
+class Deploy_OSX(unittest.TestCase):
     
     def setUp(self):
         
@@ -13,12 +15,24 @@ class Debug_OSX(unittest.TestCase):
         print "#####"
         print ""
         
-        CleanupFolder('./TNS_App');
-
+        CleanupFolder('./TNS_App')
+        GivenRealDeviceRunning(platform="ios") 
+        
     def tearDown(self):        
         pass
 
-    #TODO: Implement this test 
-    @unittest.skip("Not implemented.")  
-    def test_001_Debug_iOS(self):
-        pass
+    def test_001_Deploy_iOS_Simulator(self):
+        CreateProjectAndAddPlatform(projName="TNS_App", platform="ios", frameworkPath=iosRuntimeSymlinkPath, symlink=True)  
+        output = runAUT(tnsPath + " deploy ios --path TNS_App  --justlaunch")
+        assert ("Project successfully prepared" in output) 
+        assert ("Project successfully built" in output)   
+        assert ("Successfully deployed on device" in output)  
+        #TODO: Get device id and verify files are deployed and process is running on this device 
+
+    def test_002_Deploy_iOS_Device(self):
+        CreateProjectAndAddPlatform(projName="TNS_App", platform="ios", frameworkPath=iosRuntimeSymlinkPath, symlink=True)  
+        output = runAUT(tnsPath + " deploy ios --path TNS_App  --justlaunch")
+        assert ("Project successfully prepared" in output) 
+        assert ("Project successfully built" in output)   
+        assert ("Successfully deployed on device" in output)  
+        #TODO: Get device id and verify files are deployed and process is running on this device 
