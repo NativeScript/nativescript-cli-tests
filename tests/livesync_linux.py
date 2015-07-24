18dfa39ee0cee511ca342e6da4a3f7e9f386a459
Vasil Chimev <Vasil.Chimev@telerik.com>
2015-07-24 21:08:20 +0300

    Update.

--- tests/livesync_linux.py
+++ tests/livesync_linux.py
@@ -1,9 +1,10 @@
 import unittest
+import logging, time, threading
 
 from helpers._os_lib import CleanupFolder, runAUT, replace, catAppFile
 from helpers._tns_lib import androidRuntimePath, tnsPath, \
     CreateProjectAndAddPlatform, LiveSync, Run
-from helpers.device import GivenRunningEmulator
+from helpers.device import GivenRunningEmulator, GivenRealDeviceRunning
 
 class LiveSync_Linux(unittest.TestCase):
 
@@ -16,46 +17,90 @@ class LiveSync_Linux(unittest.TestCase):
         print ""
 
         CleanupFolder('./TNS_App')
+        # linux
         GivenRunningEmulator()
+        # osx
+        # GivenRealDeviceRunning(platform="android")
+
+    logging.basicConfig(level=logging.DEBUG,
+        format='(%(threadName)-10s) %(message)s',)
 
     def tearDown(self):
         pass
 
     def test_001_LiveSync_Android(self):
-        CreateProjectAndAddPlatform(projName = "TNS_App", platform = "android")#, frameworkPath = androidRuntimePath)
-        Run(platform = "android", path = "TNS_App")
+        CreateProjectAndAddPlatform(projName="TNS_App", platform="android", frameworkPath=androidRuntimePath)
+        Run(platform="android", path="TNS_App")
  
         replace("TNS_App/app/main-page.xml", "TAP", "TEST")
-        LiveSync(platform = "android", path = "TNS_App")
+        LiveSync(platform="android", path="TNS_App")
  
         output = catAppFile("TNSApp", "app/main-page.xml")
         assert ("<Button text=\"TEST\" tap=\"{{ tapAction }}\" />" in output)
  
     def test_002_LiveSync_Android_Device(self):
-        CreateProjectAndAddPlatform(projName = "TNS_App", platform = "android")#, frameworkPath = androidRuntimePath)
-        Run(platform = "android", path = "TNS_App")
+        CreateProjectAndAddPlatform(projName="TNS_App", platform="android", frameworkPath=androidRuntimePath)
+        Run(platform="android", path="TNS_App")
  
         replace("TNS_App/app/main-view-model.js", "taps", "clicks")
-        LiveSync(platform = "android", device = "emulator-5554", path = "TNS_App")
+        LiveSync(platform="android", device="emulator-5554", path="TNS_App")
  
         output = catAppFile("TNSApp", "app/main-view-model.js")
         assert ("this.set(\"message\", this.counter + \" clicks left\");" in output)
- 
+
     def test_003_LiveSync_Android_Watch(self):
-        pass
- 
+        CreateProjectAndAddPlatform(projName="TNS_App", platform="android")#, frameworkPath=androidRuntimePath)
+        Run(platform="android", path="TNS_App")
+
+        def watch():
+            time.sleep(38)
+            logging.debug("assert")
+#             output = catAppFile("TNSApp", "app/main-page.xml")
+#             output = runAUT("adb shell run-as org.nativescript.TNSApp cat files/app/main-page.xml", getOutput=False)
+#             assert ("<Button text=\"TEST1\" tap=\"{{ tapAction }}\" />" in output)
+
+            time.sleep(1)
+            logging.debug("replace")
+            replace("TNS_App/app/main-page.xml", "TEST1", "TEST2")
+
+            time.sleep(10)
+            logging.debug("assert")
+#             output = catAppFile("TNSApp", "app/main-page.xml")
+#             output = runAUT("adb shell run-as org.nativescript.TNSApp cat files/app/main-page.xml", getOutput=False)
+#             assert ("<Button text=\"TEST2\" tap=\"{{ tapAction }}\" />" in output)
+
+            time.sleep(1)
+            logging.debug("replace")
+            replace("TNS_App/app/main-page.xml", "TEST2", "TEST3")
+
+            time.sleep(10)
+            logging.debug("assert")
+#             output = catAppFile("TNSApp", "app/main-page.xml")
+#             output = runAUT("adb shell run-as org.nativescript.TNSApp cat files/app/main-page.xml", getOutput=False)
+#             assert ("<Button text=\"TEST3\" tap=\"{{ tapAction }}\" />" in output)
+
+        thread =  threading.Thread(target=watch)
+        thread.daemon = True
+        thread.start()
+
+        replace("TNS_App/app/main-page.xml", "TAP", "TEST1")
+        LiveSync(watch=True, path="TNS_App")
+
+        output = catAppFile("TNSApp", "app/main-page.xml")
+        assert ("<Button text=\"TEST3\" tap=\"{{ tapAction }}\" />" in output)
+
     def test_301_LiveSync(self):
-        CreateProjectAndAddPlatform(projName = "TNS_App", platform = "android")#, frameworkPath = androidRuntimePath)
-        Run(platform = "android", path = "TNS_App")
-  
+        CreateProjectAndAddPlatform(projName="TNS_App", platform="android", frameworkPath=androidRuntimePath)
+        Run(platform="android", path="TNS_App")
+
         replace("TNS_App/app/main-page.xml", "TAP", "TEST")
-        LiveSync(path = "TNS_App")
+        LiveSync(path="TNS_App")
   
         output = catAppFile("TNSApp", "app/main-page.xml")
         assert ("<Button text=\"TEST\" tap=\"{{ tapAction }}\" />" in output)
 
     def test_302_LiveSync_BeforeRun(self):
-        CreateProjectAndAddPlatform(projName = "TNS_App", platform = "android")#, frameworkPath = androidRuntimePath)
+        CreateProjectAndAddPlatform(projName="TNS_App", platform="android", frameworkPath=androidRuntimePath)
         output = runAUT(tnsPath + " livesync --path TNS_App")
 
         assert ("Project successfully prepared" in output)
--- tests/livesync_osx.py
+++ tests/livesync_osx.py
@@ -1,7 +1,7 @@
 import unittest
 
 from helpers._os_lib import CleanupFolder, runAUT, replace, catAppFile
-from helpers._tns_lib import androidRuntimePath, tnsPath, \
+from helpers._tns_lib import androidRuntimePath, iosRuntimePath, tnsPath, \
     CreateProjectAndAddPlatform, LiveSync, Run
 from helpers.device import GivenRealDeviceRunning
 
@@ -16,51 +16,62 @@ class LiveSync_OSX(unittest.TestCase):
         print ""
 
         CleanupFolder('./TNS_App');
+        GivenRealDeviceRunning(platform="ios")
         GivenRealDeviceRunning(platform="android")
 
     def tearDown(self):
         pass
 
-    def test_001_LiveSync_Android(self):
-        CreateProjectAndAddPlatform(projName = "TNS_App", platform = "android")#, frameworkPath = androidRuntimePath)
-        Run(platform = "android", path = "TNS_App")
+    def test_001_LiveSync_iOS(self):
+        CreateProjectAndAddPlatform(projName="TNS_App", platform="ios")  # , frameworkPath=iosRuntimePath)
+        Run(platform="ios", path="TNS_App")
 
         replace("TNS_App/app/main-page.xml", "TAP", "TEST")
-        LiveSync(platform = "android", path = "TNS_App")
+        LiveSync(platform="ios", path="TNS_App")
 
-        output = catAppFile("TNSApp", "app/main-page.xml")
-        assert ("<Button text=\"TEST\" tap=\"{{ tapAction }}\" />" in output)
+#         output = catAppFile("TNSApp", "app/main-page.xml")
+#         assert ("<Button text=\"TEST\" tap=\"{{ tapAction }}\" />" in output)
 
-    def test_002_LiveSync_Android_Device(self):
-        CreateProjectAndAddPlatform(projName = "TNS_App", platform = "android")#, frameworkPath = androidRuntimePath)
-        Run(platform = "android", path = "TNS_App")
+    def test_002_LiveSync_iOS_Device(self):
+        CreateProjectAndAddPlatform(projName="TNS_App", platform="ios")  # , frameworkPath=iosRuntimePath)
+        Run(platform="ios", path="TNS_App")
 
         replace("TNS_App/app/main-view-model.js", "taps", "clicks")
-        LiveSync(platform = "android", device = "030b206908e6c3c5", path = "TNS_App")
+        LiveSync(platform="ios", device="?", path="TNS_App")
 
-        output = catAppFile("TNSApp", "app/main-view-model.js")
-        assert ("this.set(\"message\", this.counter + \" clicks left\");" in output)
+#         output = catAppFile("TNSApp", "app/main-view-model.js")
+#         assert ("this.set(\"message\", this.counter + \" clicks left\");" in output)
 
-    def test_003_LiveSync_Android_Watch(self):
+    def test_003_LiveSync_iOS_Watch(self):
         pass
 
     def test_301_LiveSync(self):
-        CreateProjectAndAddPlatform(projName = "TNS_App", platform = "android")#, frameworkPath = androidRuntimePath)
-        Run(platform = "android", path = "TNS_App")
+        CreateProjectAndAddPlatform(projName="TNS_App", platform="ios")  # , frameworkPath=iosRuntimePath)
+        Run(platform="ios", path="TNS_App")
 
         replace("TNS_App/app/main-page.xml", "TAP", "TEST")
-        LiveSync(path = "TNS_App")
+        LiveSync(path="TNS_App")
 
-        output = catAppFile("TNSApp", "app/main-page.xml")
-        assert ("<Button text=\"TEST\" tap=\"{{ tapAction }}\" />" in output)
+#         output = catAppFile("TNSApp", "app/main-page.xml")
+#         assert ("<Button text=\"TEST\" tap=\"{{ tapAction }}\" />" in output)
 
     def test_302_LiveSync_BeforeRun(self):
-        CreateProjectAndAddPlatform(projName = "TNS_App", platform = "android")#, frameworkPath = androidRuntimePath)
+        CreateProjectAndAddPlatform(projName="TNS_App", platform="ios")  # , frameworkPath=iosRuntimePath)
         output = runAUT(tnsPath + " livesync --path TNS_App")
 
         assert ("Project successfully prepared" in output)
         assert ("Project successfully built" in output)
         assert ("Successfully deployed on device with identifier" in output)
 
-        output = catAppFile("TNSApp", "app/main-page.xml")
-        assert ("<Button text=\"TAP\" tap=\"{{ tapAction }}\" />" in output)
+#         output = catAppFile("TNSApp", "app/main-page.xml")
+#         assert ("<Button text=\"TAP\" tap=\"{{ tapAction }}\" />" in output)
+
+    def test_303_LiveSync_Android_Device(self):
+        CreateProjectAndAddPlatform(projName="TNS_App", platform="android")  # , frameworkPath=androidRuntimePath)
+        Run(platform="android", path="TNS_App")
+ 
+        replace("TNS_App/app/main-view-model.js", "taps", "clicks")
+        LiveSync(platform="android", device="030b206908e6c3c5", path="TNS_App")
+ 
+        output = catAppFile("TNSApp", "app/main-view-model.js")
+        assert ("this.set(\"message\", this.counter + \" clicks left\");" in output)
