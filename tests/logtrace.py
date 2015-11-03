96f5242f209f85bf4e9e9df4a52f50dee4d8aee1
Vasil Chimev <Vasil.Chimev@telerik.com>
2015-11-03 18:32:40 +0200

    Fix log trace tests.

--- tests/logtrace.py
+++ tests/logtrace.py
@@ -1,9 +1,16 @@
+'''
+Logtrace tests
+'''
 import unittest
 
-from helpers._os_lib import runAUT, CleanupFolder
-from helpers._tns_lib import tnsPath, CreateProject
+from helpers._os_lib import CleanupFolder, runAUT
+from helpers._tns_lib import CreateProject, tnsPath
 
-# pylint: disable=R0201, C0111
+# C0103 - Invalid %s name "%s"
+# C0111 - Missing docstring
+# R0201 - Method could be a function
+# R0915 - Too many statements
+# pylint: disable=C0103, C0111, R0201, R0915
 
 
 class LogTrace(unittest.TestCase):
@@ -21,22 +28,19 @@ class LogTrace(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_001_Create_Project_LogTrace(self):
+    def test_001_create_project_logtrace(self):
         output = runAUT(tnsPath + " create TNS_App --log trace")
         assert "Creating a new NativeScript project with name TNS_App and id org.nativescript.TNSApp at location" in output
         assert "Using NativeScript hello world application" in output
         assert "Copying NativeScript hello world application into" in output
         assert "Project TNS_App was successfully created" in output
 
-    def test_002_Platform_Add_LogTrace(self):
+    def test_002_platform_add_logtrace(self):
         CreateProject(projName="TNS_App")
-        output = runAUT(
-            tnsPath +
-            " platform add android --path TNS_App --log trace")
+        output = runAUT(tnsPath + " platform add android --path TNS_App --log trace")
         assert "Looking for project in" in output
         assert "Project directory is" in output
         assert "Package: org.nativescript.TNSApp" in output
         assert "Available Android targets" in output
-        assert "using Android SDK '" in output
-
+        assert "Using Android SDK" in output
         assert "Project successfully created" in output
