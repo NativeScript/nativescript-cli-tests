4acd1d51bc93825ac4c4e4a7ac32b08c5bedeabd
Vasil Chimev <Vasil.Chimev@telerik.com>
2015-11-26 14:26:30 +0200

    Update livesync_simulator_.py

--- tests/livesync_simulator_.py
+++ tests/livesync_simulator_.py
@@ -10,7 +10,7 @@ import psutil, subprocess, time, unittest
 
 from helpers._os_lib import cleanup_folder, replace
 from helpers._tns_lib import IOS_RUNTIME_SYMLINK_PATH, \
-    create_project, platform_add, run, live_sync
+    create_project, platform_add, run
 from helpers.device import stop_emulators
 from helpers.simulator import create_simulator, delete_simulator, \
     cat_app_file_on_simulator, start_simulator, stop_simulators
@@ -20,6 +20,8 @@ class LiveSyncSimulator(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+
+        # setup simulator
         stop_emulators()
         stop_simulators()
 
@@ -29,6 +31,20 @@ class LiveSyncSimulator(unittest.TestCase):
 
         start_simulator('iPhone 6s 90')
         cleanup_folder('TNS_App')
+
+        # setup app
+        create_project(proj_name="TNS_App", copy_from="data/apps/livesync-hello-world")
+        platform_add(platform="ios", framework_path=IOS_RUNTIME_SYMLINK_PATH, \
+            path="TNS_App", symlink=True)
+        run(platform="ios", emulator=True, path="TNS_App")
+
+        # replace
+        replace("TNS_App/app/main-page.xml", "TAP", "TEST")
+
+        # livesync
+        command = "tns livesync ios --emulator --watch --path TNS_App --log trace"
+        print command
+        cls.proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 
     def setUp(self):
 
@@ -41,60 +57,33 @@ class LiveSyncSimulator(unittest.TestCase):
     def tearDown(self):
         pass
 
-#     @classmethod
-#     def tearDownClass(cls):
+    @classmethod
+    def tearDownClass(cls):
+        print "Killing subprocess ..."
+        cls.proc.terminate()
+
+        time.sleep(2)
+        if psutil.pid_exists(cls.proc.pid):
+            print "Forced killing subprocess ..."
+            cls.proc.kill()
+
 #         stop_simulators()
 #         cleanup_folder('TNS_App')
 
-    def test_001_livesync_watch(self):
-        def wait_for_text_in_output(text):
-            while True:
-                line = proc.stdout.readline()
-                if text in line:
-                    print "Text \"{0}\" found in: ".format(text), line.rstrip()
-                    time.sleep(2)
-                    break
+    def wait_for_text_in_output(self, text):
+        while True:
+            line = self.proc.stdout.readline()
+            if text in line:
+                print " + Text \"{0}\" found in: ".format(text), line.rstrip()
+                time.sleep(2)
+                break
+            else:
+                print " - " + line
 
-        create_project(proj_name="TNS_App", copy_from="data/apps/livesync-hello-world")
-        platform_add(platform="ios", framework_path=IOS_RUNTIME_SYMLINK_PATH, \
-            path="TNS_App", symlink=True)
-        run(platform="ios", emulator=True, path="TNS_App")
-
-        replace("TNS_App/app/main-page.xml", "TAP", "TEST")
-        replace("TNS_App/app/main-view-model.js", "taps", "clicks")
-        replace("TNS_App/app/app.css", "30", "20")
-
-        replace("TNS_App/node_modules/tns-core-modules/LICENSE", "2015", "9999")
-        replace(
-            "TNS_App/node_modules/tns-core-modules/application/application-common.js",
-            "(\"globals\");",
-            "(\"globals\"); // test")
-
-        command = "tns livesync ios --emulator --watch --path TNS_App --log trace"
-        print command
-        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-        proc_pid = proc.pid
+    def test_001_full_livesync(self):
 
         # TODO: To be updated with console.log() when supported.
-        wait_for_text_in_output("prepared")
+        self.wait_for_text_in_output("prepared")
 
         output = cat_app_file_on_simulator("TNSApp", "app/main-page.xml")
         assert "<Button text=\"TEST\" tap=\"{{ tapAction }}\" />" in output
-        output = cat_app_file_on_simulator("TNSApp", "app/main-view-model.js")
-        assert "this.set(\"message\", this.counter + \" clicks left\");" in output
-        output = cat_app_file_on_simulator("TNSApp", "app/app.css")
-        assert "font-size: 20;" in output
-
-        output = cat_app_file_on_simulator("TNSApp", "app/tns_modules/LICENSE")
-        assert "Copyright (c) 9999 Telerik AD" in output
-        output = cat_app_file_on_simulator("TNSApp", \
-            "app/tns_modules/application/application-common.js")
-        assert "require(\"globals\"); // test" in output
-
-        print "Killing child process ..."
-        proc.terminate()
-
-        time.sleep(2)
-        if psutil.pid_exists(proc_pid):
-            print "Force killing child process ..."
-            proc.kill()
