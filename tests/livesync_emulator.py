24380b309373cb6643886d9653b47df9b69e9f3d
Vasil Chimev <Vasil.Chimev@telerik.com>
2015-12-11 17:17:53 +0200

    Update livesync_emulator.py

--- tests/livesync_emulator.py
+++ tests/livesync_emulator.py
@@ -9,8 +9,8 @@ Tests for livesync command in context of Android emulator
 
 import shutil, time
 
-from helpers._os_lib import cat_app_file_on_emulator, \
-    cleanup_folder, replace, remove
+from helpers._os_lib import create_folder, cleanup_folder, \
+    cat_app_file_on_emulator, replace #, remove
 from helpers._tns_lib import ANDROID_RUNTIME_PATH, create_project_add_platform, \
     create_project, platform_add, run, livesync, TNS_PATH
 from helpers.device import given_running_emulator, stop_emulators
@@ -33,7 +33,7 @@ class LiveSyncEmulator(Watcher):
         # start_emulator(EMULATOR_NAME)
 
         given_running_emulator()
-        cleanup_folder('./TNS_App')
+        cleanup_folder('TNS_App')
         cleanup_folder('appTest')
 
         # setup app
@@ -136,44 +136,91 @@ class LiveSyncEmulator(Watcher):
         output = cat_app_file_on_emulator("TNSApp", "app/app.css")
         assert "color: green;" in output
 
+    # Delete files
 #     def test_121_livesync_android_emulator_watch_delete_xml_file(self):
 #         remove("TNS_App/app/test/test.xml")
 #         self.wait_for_text_in_output("Page loaded 3 times.")
-# 
+#
 #         output = cat_app_file_on_emulator("TNSApp", "app/test/test.xml")
 #         assert "No such file or directory" in output
-# 
+#
 #     def test_122_livesync_android_emulator_watch_delete_js_file(self):
 #         remove("TNS_App/app/test/test.js")
 #         self.wait_for_text_in_output("Page loaded 1 times.")
-# 
+#
 #         output = cat_app_file_on_emulator("TNSApp", "app/test/test.js")
 #         assert "No such file or directory" in output
-# 
+#
 #     def test_123_livesync_android_emulator_watch_delete_css_file(self):
 #         remove("TNS_App/app/test/test.css")
 #         self.wait_for_text_in_output("Page loaded 2 times.")
-# 
+#
 #         output = cat_app_file_on_emulator("TNSApp", "app/test/test.css")
 #         assert "No such file or directory" in output
-# 
+
+    # Add files to a new folder
+    def test_131_livesync_android_emulator_watch_add_xml_file_to_new_folder(self):
+        create_folder("TNS_App/app/folder")
+        self.wait_for_text_in_output("Page loaded 1 times.")
+
+        shutil.copyfile("TNS_App/app/main-page.xml", "TNS_App/app/folder/test.xml")
+        self.wait_for_text_in_output("Page loaded 2 times.")
+
+        output = cat_app_file_on_emulator("TNSApp", "app/folder/test.xml")
+        assert "<Button text=\"WATCH\" tap=\"{{ tapAction }}\" />" in output
+
+#         remove("TNS_App/app/folder")
+#         self.wait_for_text_in_output("app/folder/") ???
+#
+#         output = cat_app_file_on_emulator("TNSApp", "app/folder/test.xml")
+#         assert "No such file or directory" in output
+
+    def test_132_livesync_android_emulator_watch_add_js_file_to_new_folder(self):
+        shutil.copyfile("TNS_App/app/app.js", "TNS_App/app/folder/test.js")
+        self.wait_for_text_in_output("Page loaded 1 times.")
+        time.sleep(2)
+
+        output = cat_app_file_on_emulator("TNSApp", "app/folder/test.js")
+        assert "application.start();" in output
+
+    def test_133_livesync_android_emulator_watch_add_css_file_to_new_folder(self):
+        shutil.copyfile("TNS_App/app/app.css", "TNS_App/app/folder/test.css")
+        self.wait_for_text_in_output("Page loaded 2 times.")
+
+        output = cat_app_file_on_emulator("TNSApp", "app/folder/test.css")
+        assert "color: green;" in output
+
+
 #     def test_301_livesync_android_emulator_before_run(self):
-#         print "~~~ Killing subprocess ..."
-#         self.process.terminate()
-# 
-#         time.sleep(2)
-#         if psutil.pid_exists(self.process.pid):
-#             print "~~~ Forced killing subprocess ..."
-#             self.process.kill()
-# 
-#         cleanup_folder('TNS_App')
+#         self.terminate_watcher()
+#         cleanup_folder('appTest')
+#
 #         create_project_add_platform(
-#             proj_name="TNS_App",
+#             proj_name="appTest",
 #             platform="android",
 #             framework_path=ANDROID_RUNTIME_PATH)
-# 
-#         replace("TNS_App/app/main-page.xml", "TAP", "TEST")
+#
+#         # replace
+#         replace("appTest/app/main-page.xml", "TAP", "TEST")
+#         replace("appTest/app/main-view-model.js", "taps", "clicks")
+#         replace("appTest/app/app.css", "30", "20")
+#
+#         replace("appTest/node_modules/tns-core-modules/LICENSE", "2015", "9999")
+#         replace(
+#             "appTest/node_modules/tns-core-modules/application/application-common.js",
+#             "(\"globals\");", "(\"globals\"); // test")
+#
 #         livesync(platform="android", device="emulator-5554", path="TNS_App")
-# 
+#
 #         output = cat_app_file_on_emulator("TNSApp", "app/main-page.xml")
 #         assert "<Button text=\"TEST\" tap=\"{{ tapAction }}\" />" in output
+#         output = cat_app_file_on_emulator("TNSApp", "app/main-view-model.js")
+#         assert "this.set(\"message\", this.counter + \" clicks left\");" in output
+#         output = cat_app_file_on_emulator("TNSApp", "app/app.css")
+#         assert "font-size: 20;" in output
+#
+#         output = cat_app_file_on_emulator("TNSApp", "app/tns_modules/LICENSE")
+#         assert "Copyright (c) 9999 Telerik AD" in output
+#         output = cat_app_file_on_emulator("TNSApp", \
+#             "app/tns_modules/application/application-common.js")
+#         assert "require(\"globals\"); // test" in output
