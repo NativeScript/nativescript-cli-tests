908e2dfa278c12b1cf721a114625aa1bb970ccb2
Vasil Chimev <Vasil.Chimev@telerik.com>
2015-12-18 12:59:57 +0200

    Updates.

--- run_tests.py
+++ run_tests.py
@@ -6,11 +6,14 @@
 """
 Entry point of functional tests
 """
-import os
-import platform
+
+
+import os, platform
+import core.cli
+
 
 from helpers._os_lib import cleanup_folder, remove, run_aut, uninstall_app, cleanup_xcode_cache
-from helpers._tns_lib import uninstall_cli, install_cli, get_android_runtime, get_ios_runtime, \
+from helpers._tns_lib import get_android_runtime, get_ios_runtime, \
     ANDROID_RUNTIME_SYMLINK_PATH, IOS_RUNTIME_SYMLINK_PATH, ANDROID_RUNTIME_PATH, IOS_RUNTIME_PATH
 from helpers.device import stop_emulators
 from helpers.simulator import stop_simulators
@@ -92,9 +95,9 @@ if __name__ == '__main__':
     cleanup_folder('tns_helloworld_app')
     cleanup_folder('node_modules')
 
-    # Uninstall previous CLI and install latest
-    uninstall_cli()
-    install_cli()
+    # uninstall/install CLI
+    core.cli.uninstall()
+    core.cli.install()
 
     # Get latest Android and iOS runtimes
     get_android_runtime()
