adbbf49394b0ed947cca367631daa95057b18762
dtopuzov <dimitar.topuzov@telerik.com>
2016-02-09 16:05:44 +0200

    Update watcher tests

--- core/osutils/watcher.py
+++ core/osutils/watcher.py
@@ -2,22 +2,28 @@
 This class implements Watcher Base Class.
 """
 
-from multiprocessing import Process
-
-import psutil
 import subprocess
 import time
 import unittest
+from multiprocessing import Process
+
+import psutil
+
+from core.osutils.file import File
+from core.settings.settings import OUTPUT_FILE, TEST_LOG
 
 
 class Watcher(unittest.TestCase):
-    SECONDS_TO_WAIT = 240
-
     @classmethod
     def start_watcher(cls, command):
+        print command
+        # remove output.txt
+        File.remove(OUTPUT_FILE)
+        # append to commads.txt
+        File.append(TEST_LOG, command)
         cls.process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 
-    def wait_for_text_in_output(self, text):
+    def wait_for_text_in_output(self, text, timeout=240):
         def read_loop():
             count = 0
             found = False
@@ -43,7 +49,7 @@ class Watcher(unittest.TestCase):
                         print (" - " + line),
                         break
 
-        self.run_with_timeout(self.SECONDS_TO_WAIT, read_loop)
+        self.run_with_timeout(timeout, read_loop)
 
     def run_with_timeout(self, timeout, func):
         if not timeout:
--- tests/livesync/livesync_emulator_watch.py
+++ tests/livesync/livesync_emulator_watch.py
@@ -6,7 +6,7 @@ from core.device.simulator import Simulator
 from core.osutils.file import File
 from core.osutils.folder import Folder
 from core.osutils.watcher import Watcher
-from core.settings.settings import ANDROID_RUNTIME_PATH, TNS_PATH, SUT_ROOT_FOLDER
+from core.settings.settings import ANDROID_RUNTIME_PATH, TNS_PATH
 from core.tns.tns import Tns
 
 
@@ -27,21 +27,6 @@ class LiveSyncEmulatorWatch(Watcher):
         Tns.platform_add(platform="android", framework_path=ANDROID_RUNTIME_PATH, path="TNS_App")
         Tns.run(platform="android", device="emulator-5554", path="TNS_App")
 
-        # replace
-        File.replace("TNS_App/app/main-page.xml", "TAP", "TEST")
-        File.replace("TNS_App/app/main-view-model.js", "taps", "clicks")
-        File.replace("TNS_App/app/app.css", "30", "20")
-
-        File.replace("TNS_App/node_modules/tns-core-modules/LICENSE", "2015", "9999")
-        File.replace(
-                "TNS_App/node_modules/tns-core-modules/application/application-common.js",
-                "(\"globals\");", "(\"globals\"); // test")
-
-        # livesync
-        command = TNS_PATH + " livesync android --emulator --device emulator-5554 --watch --path TNS_App --log trace"
-        print command
-        cls.start_watcher(command)
-
     def setUp(self):
         print ""
         print "#####"
@@ -61,6 +46,20 @@ class LiveSyncEmulatorWatch(Watcher):
         Folder.cleanup('appTest')
 
     def test_001_full_livesync_android_emulator_xml_js_css_tns_files(self):
+        # replace
+        File.replace("TNS_App/app/main-page.xml", "TAP", "TEST")
+        File.replace("TNS_App/app/main-view-model.js", "taps", "clicks")
+        File.replace("TNS_App/app/app.css", "30", "20")
+
+        File.replace("TNS_App/node_modules/tns-core-modules/LICENSE", "2015", "9999")
+        File.replace(
+                "TNS_App/node_modules/tns-core-modules/application/application-common.js",
+                "(\"globals\");", "(\"globals\"); // test")
+
+        # livesync
+        command = TNS_PATH + " livesync android --emulator --device emulator-5554 --watch --path TNS_App --log trace"
+        self.start_watcher(command)
+        time.sleep(10)
         self.wait_for_text_in_output("Page loaded 1 times.")
 
         output = Emulator.cat_app_file("TNSApp", "app/main-page.xml")
@@ -78,21 +77,21 @@ class LiveSyncEmulatorWatch(Watcher):
     # Add new files
     def test_101_livesync_android_emulator_watch_add_xml_file(self):
         shutil.copyfile("TNS_App/app/main-page.xml", "TNS_App/app/test/test.xml")
-        self.wait_for_text_in_output("Page loaded 2 times.")
+        self.wait_for_text_in_output("Page loaded 2 times.", timeout=30)
 
         output = Emulator.cat_app_file("TNSApp", "app/test/test.xml")
         assert "<Button text=\"TEST\" tap=\"{{ tapAction }}\" />" in output
 
     def test_102_livesync_android_emulator_watch_add_js_file(self):
         shutil.copyfile("TNS_App/app/app.js", "TNS_App/app/test/test.js")
-        self.wait_for_text_in_output("Page loaded 1 times.")
+        self.wait_for_text_in_output("Page loaded 1 times.", timeout=30)
 
         output = Emulator.cat_app_file("TNSApp", "app/test/test.js")
         assert "application.start();" in output
 
     def test_103_livesync_android_emulator_watch_add_css_file(self):
         shutil.copyfile("TNS_App/app/app.css", "TNS_App/app/test/test.css")
-        self.wait_for_text_in_output("Page loaded 2 times.")
+        self.wait_for_text_in_output("Page loaded 2 times.", timeout=30)
 
         output = Emulator.cat_app_file("TNSApp", "app/test/test.css")
         assert "color: #284848;" in output
@@ -100,23 +99,23 @@ class LiveSyncEmulatorWatch(Watcher):
     # Change in files
     def test_111_livesync_android_emulator_watch_change_xml_file(self):
         File.replace("TNS_App/app/main-page.xml", "TEST", "WATCH")
-        self.wait_for_text_in_output("Page loaded 3 times.")
+        self.wait_for_text_in_output("Page loaded 3 times.", timeout=30)
 
         output = Emulator.cat_app_file("TNSApp", "app/main-page.xml")
         assert "<Button text=\"WATCH\" tap=\"{{ tapAction }}\" />" in output
 
     def test_112_livesync_android_emulator_watch_change_js_file(self):
         File.replace("TNS_App/app/main-view-model.js", "clicks", "tricks")
-        self.wait_for_text_in_output("Page loaded 1 times.")
+        self.wait_for_text_in_output("Page loaded 1 times.", timeout=30)
 
         output = Emulator.cat_app_file("TNSApp", "app/main-view-model.js")
         assert "this.set(\"message\", this.counter + \" tricks left\");" in output
 
     def test_113_livesync_android_emulator_watch_change_css_file(self):
         File.replace("TNS_App/app/app.css", "#284848", "green")
-        self.wait_for_text_in_output("Page loaded 2 times.")
+        self.wait_for_text_in_output("Page loaded 2 times.", timeout=30)
 
-        output = Emulator.cat_app_file("TNSApp", "app/app.css")
+        output = Emulator.cat_app_file("TNSApp", "app/app.css", timeout=30)
         assert "color: green;" in output
 
         # Delete files
@@ -145,10 +144,10 @@ class LiveSyncEmulatorWatch(Watcher):
     # Add files to a new folder
     def test_131_livesync_android_emulator_watch_add_xml_file_to_new_folder(self):
         Folder.create("TNS_App/app/folder")
-        self.wait_for_text_in_output("Page loaded 1 times.")
+        self.wait_for_text_in_output("Page loaded 1 times.", timeout=30)
 
         shutil.copyfile("TNS_App/app/main-page.xml", "TNS_App/app/folder/test.xml")
-        self.wait_for_text_in_output("Page loaded 2 times.")
+        self.wait_for_text_in_output("Page loaded 2 times.", timeout=30)
 
         output = Emulator.cat_app_file("TNSApp", "app/folder/test.xml")
         assert "<Button text=\"WATCH\" tap=\"{{ tapAction }}\" />" in output
@@ -161,7 +160,7 @@ class LiveSyncEmulatorWatch(Watcher):
 
     def test_132_livesync_android_emulator_watch_add_js_file_to_new_folder(self):
         shutil.copyfile("TNS_App/app/app.js", "TNS_App/app/folder/test.js")
-        self.wait_for_text_in_output("Page loaded 1 times.")
+        self.wait_for_text_in_output("Page loaded 1 times.", timeout=30)
         time.sleep(2)
 
         output = Emulator.cat_app_file("TNSApp", "app/folder/test.js")
@@ -169,7 +168,7 @@ class LiveSyncEmulatorWatch(Watcher):
 
     def test_133_livesync_android_emulator_watch_add_css_file_to_new_folder(self):
         shutil.copyfile("TNS_App/app/app.css", "TNS_App/app/folder/test.css")
-        self.wait_for_text_in_output("Page loaded 2 times.")
+        self.wait_for_text_in_output("Page loaded 2 times.", timeout=30)
 
         output = Emulator.cat_app_file("TNSApp", "app/folder/test.css")
         assert "color: green;" in output
