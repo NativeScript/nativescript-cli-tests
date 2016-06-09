2f59f0cf268449ba3714d599bba2a6e45eee557d
dtopuzov <dimitar.topuzov@telerik.com>
2016-06-09 13:36:20 +0300

    Fix PEP8 errors

--- tests/emulate/emulate_android.py
+++ tests/emulate/emulate_android.py
@@ -33,7 +33,7 @@ class EmulateAndroid(unittest.TestCase):
         print "#####"
         print ""
 
-        Emulator.stop_emulators();
+        Emulator.stop_emulators()
         Folder.cleanup('./TNSAppNoPlatform')
         Folder.cleanup('./TNS_App/platforms/android/build/outputs')
 
@@ -42,18 +42,17 @@ class EmulateAndroid(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        Emulator.stop_emulators();
+        Emulator.stop_emulators()
         Folder.cleanup('./TNS_App')
 
     @unittest.skip("Skipped because of https://github.com/NativeScript/nativescript-cli/issues/352")
     def test_001_emulate_android_in_running_emulator(self):
 
-        Emulator.ensure_available();
+        Emulator.ensure_available()
 
-        Tns.create_app_platform_add(app_name="TNS_App", \
+        Tns.create_app_platform_add(app_name="TNS_App",
                                     platform="android", framework_path=ANDROID_RUNTIME_PATH)
-        output = run(TNS_PATH + " emulate android --path TNS_App --timeout 600 --justlaunch", \
-                         timeout=660)
+        output = run(TNS_PATH + " emulate android --path TNS_App --timeout 600 --justlaunch", timeout=660)
         assert "Project successfully prepared" in output
         assert "Project successfully built" in output
 
@@ -68,11 +67,11 @@ class EmulateAndroid(unittest.TestCase):
 
     def test_002_emulate_android_release(self):
 
-        output = run(TNS_PATH + " emulate android --avd Api19 " + \
-                         "--keyStorePath " + ANDROID_KEYSTORE_PATH + \
-                        " --keyStorePassword " + ANDROID_KEYSTORE_PASS + \
-                        " --keyStoreAlias " + ANDROID_KEYSTORE_ALIAS + \
-                        " --keyStoreAliasPassword " + ANDROID_KEYSTORE_ALIAS_PASS + \
+        output = run(TNS_PATH + " emulate android --avd Api19 " +
+                         "--keyStorePath " + ANDROID_KEYSTORE_PATH +
+                        " --keyStorePassword " + ANDROID_KEYSTORE_PASS +
+                        " --keyStoreAlias " + ANDROID_KEYSTORE_ALIAS +
+                        " --keyStoreAliasPassword " + ANDROID_KEYSTORE_ALIAS_PASS +
                         " --release --path TNS_App --timeout 600 --justlaunch", timeout=660)
         assert "Project successfully prepared" in output
         assert "Project successfully built" in output
@@ -91,7 +90,7 @@ class EmulateAndroid(unittest.TestCase):
 
         current_dir = os.getcwd()
         os.chdir(os.path.join(current_dir, "TNS_App"))
-        output = run(os.path.join("..", TNS_PATH) + \
+        output = run(os.path.join("..", TNS_PATH) +
                          " emulate android --avd Api19 --timeout 600 --justlaunch", timeout=660)
         os.chdir(current_dir)
         assert "Project successfully prepared" in output
@@ -105,8 +104,8 @@ class EmulateAndroid(unittest.TestCase):
 
     def test_300_emulate_android_platform_not_added(self):
         Tns.create_app(app_name="TNSAppNoPlatform")
-        output = run(TNS_PATH + \
-            " emulate android --avd Api19 --timeout 600  --justlaunch --path TNSAppNoPlatform", \
+        output = run(TNS_PATH +
+            " emulate android --avd Api19 --timeout 600  --justlaunch --path TNSAppNoPlatform",
             timeout=660)
         assert "Copying template files..." in output
         assert "Project successfully created." in output
@@ -121,7 +120,7 @@ class EmulateAndroid(unittest.TestCase):
 
     def test_400_emulate_invalid_platform(self):
         output = run(TNS_PATH + \
-            " emulate invalidPlatform --path TNS_App --timeout 600 --justlaunch", \
+            " emulate invalidPlatform --path TNS_App --timeout 600 --justlaunch",
             timeout=660)
         assert "The input is not valid sub-command for 'emulate' command" in output
         assert "Usage" in output
@@ -130,7 +129,7 @@ class EmulateAndroid(unittest.TestCase):
         "Skipped because of https://github.com/NativeScript/nativescript-cli/issues/289")
     def test_401_emulate_invalid_avd(self):
         output = run(TNS_PATH + \
-            " emulate android --avd invaliddevice_id --path TNS_App --timeout 600 --justlaunch", \
+            " emulate android --avd invaliddevice_id --path TNS_App --timeout 600 --justlaunch",
             timeout=660)
         # TODO: Modify assert when issue is fixed
         assert "'invalidPlatform' is not valid sub-command for 'emulate' command" in output
