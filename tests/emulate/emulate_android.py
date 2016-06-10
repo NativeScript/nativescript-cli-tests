723db12ca6cedb344a41afcefc41b36cdd73c178
dtopuzov <dimitar.topuzov@telerik.com>
2016-06-10 12:51:05 +0300

    Update emulator tests

--- tests/emulate/emulate_android.py
+++ tests/emulate/emulate_android.py
@@ -38,36 +38,28 @@ class EmulateAndroid(unittest.TestCase):
         Folder.cleanup('./TNS_App/platforms/android/build/outputs')
 
     def tearDown(self):
-        pass
+        Emulator.stop_emulators()
 
     @classmethod
     def tearDownClass(cls):
         Emulator.stop_emulators()
         Folder.cleanup('./TNS_App')
 
-    @unittest.skip("Skipped because of https://github.com/NativeScript/nativescript-cli/issues/352")
     def test_001_emulate_android_in_running_emulator(self):
 
         Emulator.ensure_available()
-
-        Tns.create_app_platform_add(app_name="TNS_App",
-                                    platform="android", framework_path=ANDROID_RUNTIME_PATH)
         output = run(TNS_PATH + " emulate android --path TNS_App --timeout 600 --justlaunch", timeout=660)
         assert "Project successfully prepared" in output
         assert "Project successfully built" in output
-
-        # Emulator can not be started without active UI
-        if ('ACTIVE_UI' in os.environ) and ("YES" in os.environ['ACTIVE_UI']):
-            assert "installing" in output
-            assert "running" in output
-            assert not "Starting Android emulator with image" in output
+        assert "Successfully deployed on device with identifier 'emulator-5554'" in output
+        assert not "Starting Android emulator with image" in output
 
         # TODO: Get device id and verify files are deployed and process is
         # running on this device
 
     def test_002_emulate_android_release(self):
 
-        output = run(TNS_PATH + " emulate android --avd Api19 " +
+        output = run(TNS_PATH + " emulate android --device Api19 " +
                          "--keyStorePath " + ANDROID_KEYSTORE_PATH +
                         " --keyStorePassword " + ANDROID_KEYSTORE_PASS +
                         " --keyStoreAlias " + ANDROID_KEYSTORE_ALIAS +
@@ -91,7 +83,7 @@ class EmulateAndroid(unittest.TestCase):
         current_dir = os.getcwd()
         os.chdir(os.path.join(current_dir, "TNS_App"))
         output = run(os.path.join("..", TNS_PATH) +
-                         " emulate android --avd Api19 --timeout 600 --justlaunch", timeout=660)
+                         " emulate android --device Api19 --timeout 600 --justlaunch", timeout=660)
         os.chdir(current_dir)
         assert "Project successfully prepared" in output
         assert "Project successfully built" in output
@@ -105,7 +97,7 @@ class EmulateAndroid(unittest.TestCase):
     def test_300_emulate_android_platform_not_added(self):
         Tns.create_app(app_name="TNSAppNoPlatform")
         output = run(TNS_PATH +
-            " emulate android --avd Api19 --timeout 600  --justlaunch --path TNSAppNoPlatform",
+            " emulate android --device Api19 --timeout 600  --justlaunch --path TNSAppNoPlatform",
             timeout=660)
         assert "Copying template files..." in output
         assert "Project successfully created." in output
@@ -125,12 +117,19 @@ class EmulateAndroid(unittest.TestCase):
         assert "The input is not valid sub-command for 'emulate' command" in output
         assert "Usage" in output
 
-    @unittest.skip(
-        "Skipped because of https://github.com/NativeScript/nativescript-cli/issues/289")
     def test_401_emulate_invalid_avd(self):
         output = run(TNS_PATH + \
-            " emulate android --avd invaliddevice_id --path TNS_App --timeout 600 --justlaunch",
+            " emulate android --device invaliddevice_id --path TNS_App --timeout 600 --justlaunch",
             timeout=660)
-        # TODO: Modify assert when issue is fixed
-        assert "'invalidPlatform' is not valid sub-command for 'emulate' command" in output
+        assert not "Option --avd is no longer supported. Please use --device isntead!" in output
+        assert "Cannot find device with name: invaliddevice_id" in output
+        assert "Usage" in output
+
+
+    def test_402_emulate_invalid_avd(self):
+        output = run(TNS_PATH +
+                     " emulate android --avd invaliddevice_id --path TNS_App --timeout 600 --justlaunch",
+                     timeout=660)
+        assert "Option --avd is no longer supported. Please use --device isntead!"
+        assert "Cannot find device with name: invaliddevice_id" in output
         assert "Usage" in output
