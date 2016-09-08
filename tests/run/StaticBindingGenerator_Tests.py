90ad6c44c87bbb256a39f56df7c994760f765403
plamen5kov <plamen.krasimirov.petkov@gmail.com>
2016-09-08 15:35:57 +0300

    added additional assert

--- tests/run/StaticBindingGenerator_Tests.py
+++ tests/run/StaticBindingGenerator_Tests.py
@@ -113,6 +113,9 @@ class StaticBindingGenerator(unittest.TestCase):
         threading.Timer(10, process.terminate).start()
         output = process.communicate()[0]
 
+        #make sure app hasn't crashed
+        assert "Displayed org.nativescript.TNSApp/com.tns.ErrorReportActivity" not in output, "App crashed with error activity"
+
         # check if we got called from custom activity that overrides the default one
         assert "we got called from onCreate of custom-nativescript-activity.js" in output, "Expected output not found"
 
