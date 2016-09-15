dc9fa39ccfbb31ba8d51066fd8a0eacabb8d5903
PROGRESS\koleva <koleva.pavlina@abv.bg>
2016-09-15 17:07:53 +0300

    Added logger and base_class

--- tests/other/Doctor_Tests.py
+++ tests/other/Doctor_Tests.py
@@ -1,35 +1,18 @@
 """
-Test for doctor command
+Tests for doctor command
 """
 
-import unittest
-
-from core.osutils.folder import Folder
 from core.tns.tns import Tns
+from core.base_class.BaseClass import BaseClass
 
 
-class Doctor_Tests(unittest.TestCase):
-    app_name = "TNS_App"
-
-    def setUp(self):
-
-        print ""
-        print "#####"
-        print self.id()
-        print "#####"
-        print ""
-
-        Folder.cleanup('./' + self.app_name)
-
-    def tearDown(self):
-        pass
-
+class Doctor_Tests(BaseClass):
     def test_001_doctor(self):
         output = Tns.run_tns_command("doctor", timeout=180)
         assert "No issues were detected." in output
 
     def test_200_doctor_show_warning_when_new_components_are_available(self):
-        Tns.create_app(self.app_name)
+        Tns.create_app(self.app_name, update_modules=False)
         Tns.platform_add_android(version="1.7.0", attributes={"--path": self.app_name})
         output = Tns.run_tns_command("doctor", attributes={"--path": self.app_name}, timeout=180)
         assert "Updates available" in output
