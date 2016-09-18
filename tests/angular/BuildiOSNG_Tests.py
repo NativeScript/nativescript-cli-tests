df77b218f6905f323587034e9c6cab4b45abae91
Dimitar Topuzov <dimitar.topuzov@telerik.com>
2016-09-18 12:14:31 +0300

    Update tests

--- tests/angular/BuildiOSNG_Tests.py
+++ tests/angular/BuildiOSNG_Tests.py
@@ -1,5 +1,4 @@
-import unittest
-
+from core.base_class.BaseClass import BaseClass
 from core.osutils.file import File
 from core.osutils.folder import Folder
 from core.settings.settings import IOS_RUNTIME_SYMLINK_PATH
@@ -7,51 +6,35 @@ from core.tns.tns import Tns
 from core.xcode.xcode import Xcode
 
 
-class BuildiOSNGTests(unittest.TestCase):
-    app_name = "TNS_App"
-
+class BuildiOSNGTests(BaseClass):
     @classmethod
     def setUpClass(cls):
+        super(BuildiOSNGTests, cls).setUpClass()
+
         Xcode.cleanup_cache()
         Folder.cleanup('./' + cls.app_name)
 
-        output = Tns.create_app(app_name=cls.app_name, attributes={"--ng": ""}, assert_success=False)
-        assert "successfully created" in output
+        Tns.create_app(app_name=cls.app_name, attributes={"--ng": ""})
         Tns.platform_add_ios(attributes={"--path": cls.app_name,
                                          "--frameworkPath": IOS_RUNTIME_SYMLINK_PATH,
                                          "--symlink": ""
                                          })
 
-    def setUp(self):
-        print ""
-        print "#####"
-        print self.id()
-        print "#####"
-        print ""
-
-    def tearDown(self):
-        pass
-
     @classmethod
     def tearDownClass(cls):
+        super(BuildiOSNGTests, cls).tearDownClass()
         Folder.cleanup('./' + cls.app_name)
 
     def test_010_build_ios_ng_project(self):
         output = Tns.build_ios(attributes={"--path": self.app_name})
-        assert "Project successfully prepared" in output
         assert "build/emulator/TNSApp.app" in output
-        assert "** BUILD SUCCEEDED **" in output
-        assert "Project successfully built" in output
         assert File.exists(self.app_name + "/platforms/ios/build/emulator/TNSApp.app")
 
     def test_210_build_ios_ng_project_release_fordevice(self):
         output = Tns.build_ios(attributes={"--path": self.app_name,
                                            "--for-device": "",
                                            "--release": ""})
-        assert "Project successfully prepared" in output
         assert "CONFIGURATION Release" in output
         assert "CodeSign" in output
         assert "build/device/TNSApp.app" in output
-        assert "** BUILD SUCCEEDED **" in output
-        assert "Project successfully built" in output
         assert File.exists(self.app_name + "/platforms/ios/build/device/TNSApp.ipa")
