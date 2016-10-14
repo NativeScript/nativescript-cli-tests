29ed8f5069b5c4c6c1079b7f5a5cb96631da0506
Pavlina Koleva <Pavlina.Koleva@telerik.com>
2016-10-14 13:23:18 +0300

    Fixed test 004 in PluginsAndroid suite

--- tests/plugin/PluginsAndroid_Tests.py
+++ tests/plugin/PluginsAndroid_Tests.py
@@ -84,7 +84,7 @@ class PluginsAndroidTests(BaseClass):
         Tns.plugin_add(plugin_name, attributes={"--path": self.app_name})
         assert File.exists(plugin_manifest_path)
         plugin_manifest_file = ET.parse(plugin_manifest_path)
-        root = plugin_manifest_file.getroot(plugin_manifest_path)
+        root = plugin_manifest_file.getroot()
         assert File.find_text('<manifest xmlns:android="http://schemas.android.com/apk/res/android">', plugin_manifest_path)
         assert File.find_text('<?xml version="1.0" encoding="UTF-8"?>', plugin_manifest_path)
         Tns.prepare_android(attributes={"--path": self.app_name})
