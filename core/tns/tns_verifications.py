387ec668d34435f9a56d62c8c33a56463f3a2b38
Dimitar Topuzov <dimitar.topuzov@telerik.com>
2016-12-15 20:39:20 +0200

    Fix previous commit

--- core/tns/tns_verifications.py
+++ core/tns/tns_verifications.py
@@ -4,12 +4,12 @@ Verifications for NativeScript projects.
 
 from core.osutils.file import File
 
-PLATFORM_ANDROID_APP_PATH = "/platforms/android/src/main/assets/app/"
-PLATFORM_ANDROID_NPM_MODULES_PATH = PLATFORM_ANDROID_APP_PATH + "tns_modules/"
-PLATFORM_ANDROID_TNS_MODULES_PATH = PLATFORM_ANDROID_NPM_MODULES_PATH + "tns-core-modules/"
-
 
 class TnsVerifications(object):
+    PLATFORM_ANDROID_APP_PATH = "/platforms/android/src/main/assets/app/"
+    PLATFORM_ANDROID_NPM_MODULES_PATH = PLATFORM_ANDROID_APP_PATH + "tns_modules/"
+    PLATFORM_ANDROID_TNS_MODULES_PATH = PLATFORM_ANDROID_NPM_MODULES_PATH + "tns-core-modules/"
+
     @staticmethod
     def get_ios_app_path(app_name):
         normalized_app_name = app_name.replace(' ', '')
@@ -24,11 +24,13 @@ class TnsVerifications(object):
 
     @staticmethod
     def prepared_android(app_name):
-        assert File.exists(app_name + PLATFORM_ANDROID_APP_PATH + 'main-view-model.js')
-        assert File.exists(app_name + PLATFORM_ANDROID_TNS_MODULES_PATH + 'application/application.js')
-        assert File.exists(app_name + PLATFORM_ANDROID_TNS_MODULES_PATH + 'xml/xml.js')
-        assert not File.exists(app_name + PLATFORM_ANDROID_TNS_MODULES_PATH + "application/application.android.js")
-        assert not File.exists(app_name + PLATFORM_ANDROID_TNS_MODULES_PATH + 'application/application.ios.js')
+        assert File.exists(app_name + TnsVerifications.PLATFORM_ANDROID_APP_PATH + 'main-view-model.js')
+        assert File.exists(app_name + TnsVerifications.PLATFORM_ANDROID_TNS_MODULES_PATH + 'application/application.js')
+        assert File.exists(app_name + TnsVerifications.PLATFORM_ANDROID_TNS_MODULES_PATH + 'xml/xml.js')
+        assert not File.exists(
+            app_name + TnsVerifications.PLATFORM_ANDROID_TNS_MODULES_PATH + "application/application.android.js")
+        assert not File.exists(
+            app_name + TnsVerifications.PLATFORM_ANDROID_TNS_MODULES_PATH + 'application/application.ios.js')
 
     @staticmethod
     def prepared_ios(app_name):
