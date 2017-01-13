32550507e8fc22698eb2907cf3f820bf581279bc
vchimev <Vasil.Chimev@telerik.com>
2017-01-13 13:37:47 +0200

    Update RunIOS Tests

--- tests/run/RuniOS_Tests.py
+++ tests/run/RuniOS_Tests.py
@@ -57,6 +57,10 @@ class RuniOS(BaseClass):
         # First build in release require prepare
         assert "Project successfully prepared" in output
         assert "CONFIGURATION Release" in output
+
+        assert "Successfully run application org.nativescript." in output
+        assert "Successfully started on device with identifier" in output
+
         device_ids = Device.get_ids("ios")
         for device_id in device_ids:
             assert device_id in output
@@ -69,11 +73,30 @@ class RuniOS(BaseClass):
         # First build in debug require prepare
         assert "Project successfully prepared" in output
         assert "CONFIGURATION Debug" in output
+
+        assert "Successfully transferred all files." in output
+        assert "Successfully synced application org.nativescript." in output
+
         device_ids = Device.get_ids("ios")
         for device_id in device_ids:
             assert device_id in output
 
-    def test_003_run_ios_debug_simulator(self):
+    def test_003_run_ios_debug_simulator_same_app(self):
+        output = Tns.run_ios(attributes={"--path": self.app_name,
+                                         "--justlaunch": "",
+                                         "--emulator": ""
+                                         },
+                             timeout=180)
+
+        assert "Skipping prepare." in output  # As it is prepared from test_002
+        assert "CONFIGURATION Debug" in output  # New build for emulator, as previously built only for device
+
+        assert "Successfully transferred all files." in output
+        assert "Successfully synced application org.nativescript." in output
+
+        assert Process.wait_until_running("Simulator", 60)
+
+    def test_004_run_ios_debug_simulator(self):
         output = Tns.run_ios(attributes={"--path": "\"" + self.app_name_space + "\"",
                                          "--justlaunch": "",
                                          "--emulator": ""
@@ -83,9 +106,13 @@ class RuniOS(BaseClass):
         # Prepare because this is new project
         assert "Project successfully prepared" in output
         assert "CONFIGURATION Debug" in output
+
+        assert "Successfully transferred all files." in output
+        assert "Successfully synced application org.nativescript." in output
+
         assert Process.wait_until_running("Simulator", 60)
 
-    def test_004_run_ios_release_simulator(self):
+    def test_005_run_ios_release_simulator(self):
         output = Tns.run_ios(attributes={"--path": "\"" + self.app_name_space + "\"",
                                          "--emulator": "",
                                          "--release": "",
@@ -96,14 +123,17 @@ class RuniOS(BaseClass):
         # First build for simulator in release for first time, so require prepare
         assert "Project successfully prepared" in output
         assert "CONFIGURATION Release" in output
+
+        assert "Successfully started on device with identifier" in output
+
         assert Process.wait_until_running("Simulator", 60)
         # TODO: Get device id and verify files are deployed and process is
         # running on this device
 
-    def test_005_run_ios_default(self):
+    def test_006_run_ios_default(self):
         output = Tns.run_ios(attributes={"--path": self.app_name},
                              assert_success=False,
-                             timeout=180)
+                             timeout=60)
 
         # Dimitar: Hm....not sure this is ok, but no prepare in this case.
         # Vasil: It's ok as there are no changes in this app since test_002
@@ -111,42 +141,56 @@ class RuniOS(BaseClass):
         assert "Skipping prepare." in output
         assert "Searching for devices..." in output
         assert "Skipping package build." in output
+        assert "Skipping install." in output
         assert "Refreshing application..." in output
         assert "Successfully synced application org.nativescript." in output
+
+        assert "Project successfully prepared" not in output
 
     def test_200_run_ios_inside_project(self):
         current_dir = os.getcwd()
         os.chdir(os.path.join(current_dir, self.app_name))
         output = Tns.run_ios(attributes={"--path": self.app_name,
                                          "--justlaunch": ""},
-                             tns_path=os.path.join("..", TNS_PATH), timeout=180, assert_success=False)
+                             tns_path=os.path.join("..", TNS_PATH),
+                             assert_success=False,
+                             timeout=60)
         os.chdir(current_dir)
 
         # Second build in debug should not prepare
         assert "Skipping prepare." in output
         assert "Searching for devices..." in output
         assert "Skipping package build." in output
+        assert "Skipping install." in output
         assert "Refreshing application..." in output
+        assert "Successfully synced application org.nativescript." in output
 
-        assert "Successfully synced application org.nativescript." not in output
         assert "Project successfully prepared" not in output
 
     def test_301_run_ios_platform_not_added(self):
         Tns.create_app(self.app_name_noplatform)
         output = Tns.run_ios(attributes={"--path": self.app_name_noplatform,
                                          "--justlaunch": ""},
-                             timeout=180, assert_success=False)
+                             assert_success=False,
+                             timeout=180)
         assert "Copying template files..." in output
         assert "Installing tns-ios" in output
+        assert "Project successfully created." in output
+
         assert "Project successfully prepared" in output
+        assert "Project successfully built." in output
         assert "Successfully installed on device with identifier" in output
+        assert "Successfully synced application org.nativescript." in output
 
     def test_302_run_ios_device_not_connected(self):
         output = Tns.run_ios(attributes={"--path": self.app_name_noplatform,
                                          "--device": "xxxxx",
                                          "--justlaunch": ""},
-                             timeout=180, assert_success=False)
+                             assert_success=False,
+                             timeout=60)
+        assert "Skipping prepare." in output
         assert "Cannot resolve the specified connected device" in output
+
         assert "Project successfully prepared" not in output
         assert "Project successfully built" not in output
         assert "Successfully deployed on device" not in output
