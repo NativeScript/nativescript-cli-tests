62618382426fe7c9ab5704f9c26ae0f12bfcaca2
dtopuzov <dtopuzov@gmail.com>
2017-03-17 15:44:06 +0200

    Update debug tests

---
+++ tests/device/debug_android_tests.py
@@ -0,0 +1,13 @@
+"""
+Test for Android debugger
+"""
+import unittest
+
+from core.base_class.BaseClass import BaseClass
+
+
+class DebugAndroid(BaseClass):
+    @unittest.skip("Not implemented.")
+    def test_001_debug_android(self):
+        # TODO: Implement this test
+        pass
---
+++ tests/device/debug_ios_tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+from core.base_class.BaseClass import BaseClass
+
+
+class DebugiOS(BaseClass):
+    @unittest.skip("Not implemented.")
+    def test_001_debug_ios(self):
+        # TODO: Implement this test
+        pass
