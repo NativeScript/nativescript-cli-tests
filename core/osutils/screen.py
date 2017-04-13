84e79aa8f538f6754a7b93a87cc02a3b25782b9e
dtopuzov <dtopuzov@gmail.com>
2017-04-13 09:30:21 +0300

    Temporary disable screen capture on Linux (until we fix it)

--- core/osutils/screen.py
+++ core/osutils/screen.py
@@ -1,5 +1,8 @@
 from PIL import ImageGrab
 
+from core.osutils.os_type import OSType
+from core.settings.settings import CURRENT_OS
+
 
 class Screen(object):
     @staticmethod
@@ -8,6 +11,10 @@ class Screen(object):
         Save screen of host machine.
         :param path: Path where screen will be saved.
         """
-        print 'Save current host screen at {0}'.format(path)
-        im = ImageGrab.grab()
-        im.save(path)
+
+        if CURRENT_OS is OSType.LINUX:
+            raise NotImplementedError
+        else:
+            print 'Save current host screen at {0}'.format(path)
+            im = ImageGrab.grab()
+            im.save(path)
