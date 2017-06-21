a772f4fe9aef57b0f0e6bc7f3ca60c90ab4063c2
Dimitar Topuzov <dimitar.topuzov@telerik.com>
2017-06-21 13:22:51 +0300

    Update test_103_check_android_manifest_merged

--- tests/build/android/plugin_android_tests.py
+++ tests/build/android/plugin_android_tests.py
@@ -82,7 +82,7 @@ class PluginsAndroidTests(BaseClass):
         assert File.exists(src_manifest)
         Tns.build_android(attributes={"--path": self.app_name})
         assert File.pattern_exists(res_manifest, "AndroidManifest.xml")
-        merged_manifest_file = ET.parse(os.path.join(res_manifest, "full", "F0F1", "debug", "AndroidManifest.xml"))
+        merged_manifest_file = ET.parse(os.path.join(res_manifest, "full", "F1F2", "debug", "AndroidManifest.xml"))
         root2 = merged_manifest_file.getroot()
         res = False
         for child in root:
