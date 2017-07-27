17bec168e1a4b58a48d4c0a44a82fa0bf21e73a2
dtopuzov <dtopuzov@gmail.com>
2017-07-27 14:56:09 +0300

    Remove tests for screen builder templates

--- tests/templates/getting_started_templates_tests.py
+++ tests/templates/getting_started_templates_tests.py
@@ -1,14 +1,5 @@
 """
-Test for `tns run android` command.
-
-Run should sync all the changes correctly:
- - Valid changes in CSS, JS, XML should be applied.
- - Invalid changes in XML should be logged in the console and lives-sync should continue when XML is fixed.
- - Hidden files should not be synced at all.
- - --syncAllFiles should sync changes in node_modules
- - --justlaunch should release the console.
-
-If emulator is not started and device is not connected `tns run android` should start emulator.
+Verify getting started templates looks ok
 """
 
 import os
@@ -18,18 +9,21 @@ from nose_parameterized import parameterized
 from core.base_class.BaseClass import BaseClass
 from core.device.device import Device
 from core.device.emulator import Emulator
+from core.device.simulator import Simulator
 from core.osutils.folder import Folder
-from core.settings.settings import ANDROID_RUNTIME_PATH, EMULATOR_ID, EMULATOR_NAME
+from core.settings.settings import ANDROID_RUNTIME_PATH, EMULATOR_ID, EMULATOR_NAME, IOS_RUNTIME_PATH, SIMULATOR_NAME
 from core.tns.tns import Tns
 
 
-class TemplateAndroidTests(BaseClass):
+class GettingStartedTemplatesTests(BaseClass):
     @classmethod
     def setUpClass(cls):
         logfile = os.path.join('out', cls.__name__ + '.txt')
         BaseClass.setUpClass(logfile)
         Emulator.stop()
         Emulator.ensure_available()
+        Simulator.stop()
+        cls.SIMULATOR_ID = Simulator.ensure_available(simulator_name=SIMULATOR_NAME)
         Folder.cleanup(cls.app_name)
 
     def setUp(self):
@@ -47,33 +41,28 @@ class TemplateAndroidTests(BaseClass):
     @parameterized.expand([
         "nativescript-template-tutorial",
         "nativescript-template-ng-tutorial",
-        "template-drawer-navigation-ts",
-        "template-tab-navigation-ts",
-        "template-master-detail-ng",
-        "template-drawer-navigation",
-        "template-master-detail-ts",
-        "template-tab-navigation",
-        "template-drawer-navigation-ng",
-        "template-master-detail",
-        "template-tab-navigation-ng",
     ])
     def test_100_templates_android(self, template_source):
-        """Tests for {N} templates"""
 
         # Create application
         url = "https://github.com/NativeScript/" + template_source
-        template = str(template_source).replace("template-", "").replace("-ng", "").replace("-ts", "").replace("-", "")
-        Tns.create_app(self.app_name, attributes={"--template": url, "--appid": "org.nativescript." + template},
-                       assert_success=False)
+        Tns.create_app(self.app_name, attributes={"--template": url}, assert_success=True)
         Tns.platform_add_android(attributes={'--path': self.app_name, '--frameworkPath': ANDROID_RUNTIME_PATH})
+        Tns.platform_add_ios(attributes={'--path': self.app_name, '--frameworkPath': IOS_RUNTIME_PATH})
 
         # Build it
         Tns.build_android(attributes={'--path': self.app_name})
+        Tns.build_ios(attributes={'--path': self.app_name})
 
-        # Start it
+        # Verify Android looks ok
         output = Tns.run_android(attributes={'--path': self.app_name, '--emulator': '', '--justlaunch': ''},
                                  assert_success=False)
         assert "Successfully synced application" in output, "Failed to run the app!"
-
-        # Verify app looks correct inside emulator
         Device.screen_match(device_name=EMULATOR_NAME, device_id=EMULATOR_ID, expected_image=template_source + "_home")
+
+        # Verify iOS looks ok
+        output = Tns.run_ios(attributes={'--path': self.app_name, '--emulator': '', '--justlaunch': ''},
+                             assert_success=False)
+        assert "Successfully synced application" in output, "Failed to run the app!"
+        Device.screen_match(device_name=SIMULATOR_NAME, device_id=self.SIMULATOR_ID,
+                            expected_image=template_source + '_home')
