17d668a3dec119450343d4c2aa793f53e4b002dd
vchimev <Vasil.Chimev@telerik.com>
2017-10-24 10:40:07 +0300

    refactor(perf): utils

--- core/utils/perf_utils.py
+++ core/utils/perf_utils.py
@@ -8,7 +8,7 @@ import pandas as pd
 file_path = sys.argv[1]  # 'perf_info.csv'
 file_name = file_path.replace('.csv', '')
 df = pd.read_csv(file_path)
-data = df[['datetime', 'start_time']].loc[df['hostname'] == 'mcsofnsbuild07'].tail(15)
+data = df[['datetime', 'start_time']].loc[df['apk_name'] == 'template-hello-world-ng-release'].tail(15)
 plot = data.plot(x='datetime', y='start_time', legend=False,
                  linewidth=5, rot=23, title='start_time')
 fig = plot.get_figure()
