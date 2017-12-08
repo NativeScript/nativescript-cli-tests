cdaf57ac8a1ce34357b7da80d41aa17f443f235b
dtopuzov <dtopuzov@gmail.com>
2017-12-08 13:46:34 +0200

    Fix tests

--- tests/build/common/stderr_output_tests.py
+++ tests/build/common/stderr_output_tests.py
@@ -4,12 +4,10 @@ Tests for error output.
 
 from core.base_class.BaseClass import BaseClass
 from core.osutils.file import File
-from core.settings.strings import invalid, invalid_input, invalid_option
 from core.tns.tns import Tns
 
 
 class StderrOutputTests(BaseClass):
-
     @classmethod
     def setUpClass(cls):
         BaseClass.setUpClass(cls.__name__)
@@ -20,9 +18,9 @@ class StderrOutputTests(BaseClass):
 
     def test_001_redirect_strerr_output_to_file(self):
         File.remove('./stderr.txt')
-        output = Tns.run_tns_command("run " + invalid, attributes={" 2> stderr.txt ": ""})
-        assert invalid_input.format("run") in output
+        output = Tns.run_tns_command("run invalidEntry", attributes={" 2> stderr.txt ": ""})
+        assert "No project found at or above" in output
 
     def test_002_command_option_validation(self):
-        output = Tns.run_tns_command("create " + self.app_name, attributes={"--" + invalid: "tns-app"})
-        assert invalid_option.format(invalid) in output
+        output = Tns.run_tns_command("create " + self.app_name, attributes={"--invalidEntry": "tns-app"})
+        assert "The option 'invalidEntry' is not supported." in output
