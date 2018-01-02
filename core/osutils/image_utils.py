d6ee500297d2f98b093192ed017ab2084b73481d
dtopuzov <dtopuzov@gmail.com>
2018-01-02 14:48:36 +0200

    refactor: count similar pixels as equal

--- core/osutils/image_utils.py
+++ core/osutils/image_utils.py
@@ -2,7 +2,6 @@ from PIL import Image
 
 
 class ImageUtils(object):
-
     @staticmethod
     def image_match(actual_image_path, expected_image_path, tolerance=0.05):
         """
@@ -24,9 +23,18 @@ class ImageUtils(object):
         diff_image = actual_image.copy()
         for x in range(0, width):
             for y in range(40, height):
-                if actual_pixels[x, y] != expected_pixels[x, y]:
-                    diff_pixels += 1
-                    diff_image.load()[x, y] = (255, 0, 0)
+                actual_pixel = actual_pixels[x, y]
+                expected_pixel = expected_pixels[x, y]
+                if actual_pixel != expected_pixel:
+                    actual_r = actual_pixel[0]
+                    actual_g = actual_pixel[1]
+                    actual_b = actual_pixel[2]
+                    expected_r = expected_pixel[0]
+                    expected_g = expected_pixel[1]
+                    expected_b = expected_pixel[2]
+                    if abs(actual_r + actual_g + actual_b - expected_r - expected_g - expected_b) > 15:
+                        diff_pixels += 1
+                        diff_image.load()[x, y] = (255, 0, 0)
 
         diff_percent = 100 * float(diff_pixels) / total_pixels
         if diff_percent < tolerance:
