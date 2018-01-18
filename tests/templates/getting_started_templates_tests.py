f6fb0270e2e28da93866631359fb476367258911
dtopuzov <dtopuzov@gmail.com>
2018-01-18 17:23:16 +0200

    refactor: simplify tests for getting  started tutorials

--- tests/templates/getting_started_templates_tests.py
+++ tests/templates/getting_started_templates_tests.py
@@ -9,7 +9,7 @@ from core.device.device import Device
 from core.device.emulator import Emulator
 from core.device.simulator import Simulator
 from core.osutils.folder import Folder
-from core.settings.settings import ANDROID_RUNTIME_PATH, EMULATOR_ID, EMULATOR_NAME, IOS_RUNTIME_PATH, SIMULATOR_NAME
+from core.settings.settings import ANDROID_RUNTIME_PATH, EMULATOR_ID, IOS_RUNTIME_PATH, SIMULATOR_NAME
 from core.tns.tns import Tns
 
 
@@ -49,6 +49,8 @@ class GettingStartedTemplatesTests(BaseClass):
         if update:
             Tns.platform_add_android(attributes={'--path': self.app_name, '--frameworkPath': ANDROID_RUNTIME_PATH})
             Tns.platform_add_ios(attributes={'--path': self.app_name, '--frameworkPath': IOS_RUNTIME_PATH})
+            if "-ng-" in description:
+                Tns.update_angular(self.app_name)
         else:
             Tns.platform_add_android(attributes={'--path': self.app_name}, version="latest")
             Tns.platform_add_ios(attributes={'--path': self.app_name}, version="latest")
@@ -61,11 +63,10 @@ class GettingStartedTemplatesTests(BaseClass):
         output = Tns.run_android(attributes={'--path': self.app_name, '--emulator': '', '--justlaunch': ''},
                                  assert_success=False)
         assert "Successfully synced application" in output, "Failed to run the app!"
-        Device.screen_match(device_name=EMULATOR_NAME, device_id=EMULATOR_ID, expected_image=description, tolerance=3.0)
+        Device.wait_for_text(device_id=EMULATOR_ID, text="My App")
 
         # Verify iOS looks ok
         output = Tns.run_ios(attributes={'--path': self.app_name, '--emulator': '', '--justlaunch': ''},
                              assert_success=False)
         assert "Successfully synced application" in output, "Failed to run the app!"
-        Device.screen_match(device_name=SIMULATOR_NAME, device_id=self.SIMULATOR_ID, expected_image=description,
-                            tolerance=3.0)
+        Device.wait_for_text(device_id=self.SIMULATOR_ID, text="My App")
