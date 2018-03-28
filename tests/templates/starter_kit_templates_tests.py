f76fa4ff9c9dbf6d0fc2c4e70713226dc6939152
dtopuzov <dtopuzov@gmail.com>
2018-03-28 18:24:50 +0300

    updatE: starter kits livesync tests

--- tests/templates/starter_kit_templates_tests.py
+++ tests/templates/starter_kit_templates_tests.py
@@ -6,8 +6,8 @@ import unittest
 from nose_parameterized import parameterized
 
 from core.base_class.BaseClass import BaseClass
-from core.device.device import Device
 from core.device.emulator import Emulator
+from core.device.helpers.adb import Adb
 from core.device.simulator import Simulator
 from core.npm.npm import Npm
 from core.osutils.os_type import OSType
@@ -15,30 +15,85 @@ from core.settings.settings import ANDROID_PACKAGE, IOS_PACKAGE, TYPESCRIPT_PACK
     CURRENT_OS, WEBPACK_PACKAGE, EMULATOR_ID, SIMULATOR_NAME, SASS_PACKAGE
 from core.tns.replace_helper import ReplaceHelper
 from core.tns.tns import Tns
+from core.tns.tns_platform_type import Platform
 from tests.webpack.helpers.helpers import Helpers
 
 
 class StarterKitsTemplateTests(BaseClass):
     DEMOS = [
         'template-master-detail',
-        'template-master-detail-ts',
+        # 'template-master-detail-ts',
         'template-master-detail-ng'
     ]
 
     xml_change = ['app/cars/cars-list-page.xml', 'Browse', 'Best Car Ever!']
     html_change = ['app/cars/car-list.component.html', 'Browse', 'Best Car Ever!']
-    js_change = ['app/cars/shared/car-model.js', 'name: options.name,', 'name: options.transmission,']
-    ts_change = ['app/cars/shared/car-model.ts', 'this._name = options.name;', 'this._name = options.transmission;']
-    ts_change_ng = ['app/cars/shared/car.model.ts', 'this.name = options.name;', 'this.name = options.transmission;']
+    js_change = ['app/cars/shared/car-model.js', 'name: options.name,', 'name: "SyncJSTest",']
+    ts_change = ['app/cars/shared/car-model.ts', 'this._name = options.name;', 'this._name = "SyncJSTest";']
+    ts_change_ng = ['app/cars/shared/car.model.ts', 'this.name = options.name;', 'this.name = "SyncJSTest";']
     sass_root_level_variable_change = ['app/_app-variables.scss', '$accent-dark: #3A53FF !default;',
-                                       '$accent-dark: #FFFFFF !default;']
+                                       '$accent-dark: #FF6666 !default;']
     sass_root_level_android_change = ["app/app.android.scss", "@import 'app-common';",
-                                      "@import 'app-common'; .text-primary {color: $accent-dark;}"]
+                                      "@import 'app-common'; .text-primary {color: black;}"]
     sass_root_level_ios_change = ["app/app.ios.scss", "@import 'app-common';",
-                                  "@import 'app-common'; .text-primary {color: $accent-dark;}"]
+                                  "@import 'app-common'; .text-primary {color: black;}"]
     sass_nested_level_change = ['app/cars/_cars-list-common.scss', 'padding: 8 15 4 15;', 'padding: 50 50 50 50;']
-
+    sass_nested_level_change_ng = ['app/cars/_car-list.component.scss', 'padding: 8 15 4 15;', 'padding: 50 50 50 50;']
     SIMULATOR_ID = ""
+
+    @staticmethod
+    def apply_changes(self, demo, platform):
+        if '-ng' in demo:
+            ReplaceHelper.replace(demo, self.ts_change_ng)
+            assert Adb.wait_for_text(device_id=EMULATOR_ID, text="SyncJSTest"), "Failed to apply TS changes"
+            ReplaceHelper.replace(demo, self.html_change)
+            assert Adb.wait_for_text(device_id=EMULATOR_ID, text="Best Car Ever!"), "Failed to apply XML changes!"
+        elif '-ts' in demo:
+            ReplaceHelper.replace(demo, self.ts_change)
+            assert Adb.wait_for_text(device_id=EMULATOR_ID, text="SyncJSTest"), "Failed to apply TS changes"
+            ReplaceHelper.replace(demo, self.xml_change)
+            assert Adb.wait_for_text(device_id=EMULATOR_ID, text="Best Car Ever!"), "Failed to apply XML changes!"
+        else:
+            ReplaceHelper.replace(demo, self.js_change)
+            assert Adb.wait_for_text(device_id=EMULATOR_ID, text="SyncJSTest"), "Failed to apply JS changes"
+            ReplaceHelper.replace(demo, self.xml_change)
+            assert Adb.wait_for_text(device_id=EMULATOR_ID, text="Best Car Ever!"), "Failed to apply XML changes!"
+        ReplaceHelper.replace(demo, self.sass_root_level_variable_change)
+        if platform == Platform.ANDROID:
+            ReplaceHelper.replace(demo, self.sass_root_level_android_change)
+        else:
+            ReplaceHelper.replace(demo, self.sass_root_level_ios_change)
+        if '-ng' in demo:
+            ReplaceHelper.replace(demo, self.sass_nested_level_change_ng)
+        else:
+            ReplaceHelper.replace(demo, self.sass_nested_level_change)
+
+    @staticmethod
+    def revert_changes(self, demo, platform):
+        if '-ng' in demo:
+            ReplaceHelper.rollback(demo, self.ts_change_ng)
+            assert Adb.wait_for_text(device_id=EMULATOR_ID, text="BMW"), "Failed to rollback TS changes"
+            ReplaceHelper.rollback(demo, self.html_change)
+            assert Adb.wait_for_text(device_id=EMULATOR_ID, text="Browse"), "Failed to rollback XML changes!"
+        elif '-ts' in demo:
+            ReplaceHelper.rollback(demo, self.ts_change)
+            assert Adb.wait_for_text(device_id=EMULATOR_ID, text="BMW"), "Failed to rollback TS changes"
+            ReplaceHelper.rollback(demo, self.xml_change)
+            assert Adb.wait_for_text(device_id=EMULATOR_ID, text="Browse"), "Failed to rollback XML changes!"
+        else:
+            ReplaceHelper.rollback(demo, self.js_change)
+            assert Adb.wait_for_text(device_id=EMULATOR_ID, text="BMW"), "Failed to rollback JS changes"
+            ReplaceHelper.rollback(demo, self.xml_change)
+            assert Adb.wait_for_text(device_id=EMULATOR_ID, text="Browse"), "Failed to rollback XML changes!"
+        ReplaceHelper.rollback(demo, self.sass_root_level_variable_change)
+        if platform == Platform.ANDROID:
+            ReplaceHelper.rollback(demo, self.sass_root_level_android_change)
+        else:
+            ReplaceHelper.rollback(demo, self.sass_root_level_ios_change)
+        if '-ng' in demo:
+            ReplaceHelper.rollback(demo, self.sass_nested_level_change_ng)
+        else:
+            ReplaceHelper.rollback(demo, self.sass_nested_level_change)
 
     @classmethod
     def setUpClass(cls):
@@ -87,39 +142,15 @@ class StarterKitsTemplateTests(BaseClass):
                          timeout=180)
         Helpers.android_screen_match(image=demo + '_home', tolerance=1.0)
 
-        # Change JS, XML and CSS
-        if '-ng' in demo:
-            ReplaceHelper.replace(demo, self.ts_change_ng)
-            ReplaceHelper.replace(demo, self.html_change)
-        if '-ts' in demo:
-            ReplaceHelper.replace(demo, self.ts_change)
-            ReplaceHelper.replace(demo, self.xml_change)
-        else:
-            ReplaceHelper.replace(demo, self.js_change)
-            ReplaceHelper.replace(demo, self.xml_change)
-            assert Device.wait_for_text("Best Car Ever!"), "XML Changes not applied!"
-        ReplaceHelper.replace(demo, self.sass_root_level_variable_change)
-        ReplaceHelper.replace(demo, self.sass_root_level_android_change)
-        ReplaceHelper.replace(demo, self.sass_nested_level_change)
+        # Apply changes
+        StarterKitsTemplateTests.apply_changes(self=self, demo=demo, platform=Platform.ANDROID)
 
         # Verify application looks correct
         Tns.wait_for_log(log_file=log, string_list=Helpers.no_wp_sync, not_existing_string_list=Helpers.wp_errors)
         Helpers.android_screen_match(image=demo + '_sync')
 
         # Revert changes
-        if '-ng' in demo:
-            ReplaceHelper.rollback(demo, self.ts_change_ng)
-            ReplaceHelper.rollback(demo, self.html_change)
-        if '-ts' in demo:
-            ReplaceHelper.rollback(demo, self.ts_change)
-            ReplaceHelper.rollback(demo, self.xml_change)
-        else:
-            ReplaceHelper.rollback(demo, self.js_change)
-            ReplaceHelper.rollback(demo, self.xml_change)
-            assert Device.wait_for_text("Browse"), "XML Changes not applied!"
-        ReplaceHelper.rollback(demo, self.sass_root_level_variable_change)
-        ReplaceHelper.rollback(demo, self.sass_root_level_android_change)
-        ReplaceHelper.rollback(demo, self.sass_nested_level_change)
+        StarterKitsTemplateTests.revert_changes(self=self, demo=demo, platform=Platform.ANDROID)
 
         # Verify application looks correct
         Tns.wait_for_log(log_file=log, string_list=Helpers.no_wp_sync, not_existing_string_list=Helpers.wp_errors)
@@ -135,37 +166,15 @@ class StarterKitsTemplateTests(BaseClass):
         Helpers.ios_screen_match(sim_id=self.SIMULATOR_ID, image=demo + '_home', tolerance=1.0)
         Helpers.wait_webpack_watcher()
 
-        # Change JS, XML and CSS
-        if '-ng' in demo:
-            ReplaceHelper.replace(demo, self.ts_change_ng)
-            ReplaceHelper.replace(demo, self.html_change)
-        if '-ts' in demo:
-            ReplaceHelper.replace(demo, self.ts_change)
-            ReplaceHelper.replace(demo, self.xml_change)
-        else:
-            ReplaceHelper.replace(demo, self.js_change)
-            ReplaceHelper.replace(demo, self.xml_change)
-        ReplaceHelper.replace(demo, self.sass_root_level_variable_change)
-        ReplaceHelper.replace(demo, self.sass_root_level_ios_change)
-        ReplaceHelper.replace(demo, self.sass_nested_level_change)
+        # Apply changes
+        StarterKitsTemplateTests.apply_changes(self=self, demo=demo, platform=Platform.IOS)
 
         # Verify application looks correct
         Tns.wait_for_log(log_file=log, string_list=Helpers.no_wp_sync, not_existing_string_list=Helpers.wp_errors)
         Helpers.ios_screen_match(sim_id=self.SIMULATOR_ID, image=demo + '_sync')
 
         # Revert changes
-        if '-ng' in demo:
-            ReplaceHelper.rollback(demo, self.ts_change_ng)
-            ReplaceHelper.rollback(demo, self.html_change)
-        if '-ts' in demo:
-            ReplaceHelper.rollback(demo, self.ts_change)
-            ReplaceHelper.rollback(demo, self.xml_change)
-        else:
-            ReplaceHelper.rollback(demo, self.js_change)
-            ReplaceHelper.rollback(demo, self.xml_change)
-        ReplaceHelper.rollback(demo, self.sass_root_level_variable_change)
-        ReplaceHelper.rollback(demo, self.sass_root_level_ios_change)
-        ReplaceHelper.rollback(demo, self.sass_nested_level_change)
+        StarterKitsTemplateTests.revert_changes(self=self, demo=demo, platform=Platform.IOS)
 
         # Verify application looks correct
         Tns.wait_for_log(log_file=log, string_list=Helpers.no_wp_sync, not_existing_string_list=Helpers.wp_errors)
@@ -182,37 +191,15 @@ class StarterKitsTemplateTests(BaseClass):
         Helpers.android_screen_match(image=demo + '_home', tolerance=1.0)
         Helpers.wait_webpack_watcher()
 
-        # Change JS, XML and CSS
-        if '-ng' in demo:
-            ReplaceHelper.replace(demo, self.ts_change_ng)
-            ReplaceHelper.replace(demo, self.html_change)
-        if '-ts' in demo:
-            ReplaceHelper.replace(demo, self.ts_change)
-            ReplaceHelper.replace(demo, self.xml_change)
-        else:
-            ReplaceHelper.replace(demo, self.js_change)
-            ReplaceHelper.replace(demo, self.xml_change)
-        ReplaceHelper.replace(demo, self.sass_root_level_variable_change)
-        ReplaceHelper.replace(demo, self.sass_root_level_android_change)
-        ReplaceHelper.replace(demo, self.sass_nested_level_change)
+        # Apply changes
+        StarterKitsTemplateTests.apply_changes(self=self, demo=demo, platform=Platform.ANDROID)
 
         # Verify application looks correct
         Tns.wait_for_log(log_file=log, string_list=Helpers.wp_sync, not_existing_string_list=Helpers.wp_errors)
         Helpers.android_screen_match(image=demo + '_sync')
 
         # Revert changes
-        if '-ng' in demo:
-            ReplaceHelper.rollback(demo, self.ts_change_ng)
-            ReplaceHelper.rollback(demo, self.html_change)
-        if '-ts' in demo:
-            ReplaceHelper.rollback(demo, self.ts_change)
-            ReplaceHelper.rollback(demo, self.xml_change)
-        else:
-            ReplaceHelper.rollback(demo, self.js_change)
-            ReplaceHelper.rollback(demo, self.xml_change)
-        ReplaceHelper.rollback(demo, self.sass_root_level_variable_change)
-        ReplaceHelper.rollback(demo, self.sass_root_level_android_change)
-        ReplaceHelper.rollback(demo, self.sass_nested_level_change)
+        StarterKitsTemplateTests.revert_changes(self=self, demo=demo, platform=Platform.ANDROID)
 
         # Verify application looks correct
         Tns.wait_for_log(log_file=log, string_list=Helpers.wp_sync, not_existing_string_list=Helpers.wp_errors)
@@ -229,37 +216,15 @@ class StarterKitsTemplateTests(BaseClass):
         Helpers.ios_screen_match(sim_id=self.SIMULATOR_ID, image=demo + '_home', tolerance=1.0)
         Helpers.wait_webpack_watcher()
 
-        # Change JS, XML and CSS
-        if '-ng' in demo:
-            ReplaceHelper.replace(demo, self.ts_change_ng)
-            ReplaceHelper.replace(demo, self.html_change)
-        if '-ts' in demo:
-            ReplaceHelper.replace(demo, self.ts_change)
-            ReplaceHelper.replace(demo, self.xml_change)
-        else:
-            ReplaceHelper.replace(demo, self.js_change)
-            ReplaceHelper.replace(demo, self.xml_change)
-        ReplaceHelper.replace(demo, self.sass_root_level_variable_change)
-        ReplaceHelper.replace(demo, self.sass_root_level_ios_change)
-        ReplaceHelper.replace(demo, self.sass_nested_level_change)
+        # Apply changes
+        StarterKitsTemplateTests.apply_changes(self=self, demo=demo, platform=Platform.IOS)
 
         # Verify application looks correct
         Tns.wait_for_log(log_file=log, string_list=Helpers.wp_sync, not_existing_string_list=Helpers.wp_errors)
         Helpers.ios_screen_match(sim_id=self.SIMULATOR_ID, image=demo + '_sync')
 
         # Revert changes
-        if '-ng' in demo:
-            ReplaceHelper.rollback(demo, self.ts_change_ng)
-            ReplaceHelper.rollback(demo, self.html_change)
-        if '-ts' in demo:
-            ReplaceHelper.rollback(demo, self.ts_change)
-            ReplaceHelper.rollback(demo, self.xml_change)
-        else:
-            ReplaceHelper.rollback(demo, self.js_change)
-            ReplaceHelper.rollback(demo, self.xml_change)
-        ReplaceHelper.rollback(demo, self.sass_root_level_variable_change)
-        ReplaceHelper.rollback(demo, self.sass_root_level_ios_change)
-        ReplaceHelper.rollback(demo, self.sass_nested_level_change)
+        StarterKitsTemplateTests.revert_changes(self=self, demo=demo, platform=Platform.IOS)
 
         # Verify application looks correct
         Tns.wait_for_log(log_file=log, string_list=Helpers.wp_sync, not_existing_string_list=Helpers.wp_errors)
