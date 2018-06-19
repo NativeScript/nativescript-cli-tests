e055c1116b5f680375eb0309009c34889457d71c
Natalia-Hristova <natalia.hristova@telerik.com>
2018-06-19 18:07:40 +0300

    fix: add missed file

---
+++ data/issues/nativescript-cli-3650/main-page.js
@@ -0,0 +1,11 @@
+var vmModule = require("./main-view-model");
+var count = 0;
+function pageLoaded(args) {
+    var page = args.object;
+    page.bindingContext = vmModule.mainViewModel;
+    console.log("Page loaded " + ++count + " times.");
+    
+    var objTC = new TestClass();
+    objTC.sayHey();
+}
+exports.pageLoaded = pageLoaded;
