403de89fc9cf0296522a86f87d572eeab39bb805
vhristov5555 <vasil.hristov@progress.com>
2018-07-25 13:32:07 +0300

    Add additional fix for runtime tests.

--- data/issues/android-runtime-1046/main-page.js
+++ data/issues/android-runtime-1046/main-page.js
@@ -4,6 +4,16 @@ var AlbumSelectActivity = $in.myinnos.awesomeimagepicker.activities.AlbumSelectA
 function onNavigatingTo(args) {
     var page = args.object;
     page.bindingContext = createViewModel();
+    sleep(2000);
     console.log("###TEST PASSED###");
 }
 exports.onNavigatingTo = onNavigatingTo;
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
--- data/issues/android-runtime-1060/main-page.js
+++ data/issues/android-runtime-1060/main-page.js
@@ -5,6 +5,16 @@ function onNavigatingTo(args) {
     page.bindingContext = createViewModel();
     var bb = java.nio.ByteBuffer.allocate(12);
     var ab = ArrayBuffer.from(bb);
+    sleep(2000);
     console.log("###TEST PASSED###");
 }
 exports.onNavigatingTo = onNavigatingTo;
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
