8fa687910b003a8120be926a6f5e19ab5aa3f99a
Maria Endarova <endarova@progress.com>
2018-10-30 11:30:51 +0200

    add new test for android preview

--- tests/preview_tests/preview_android_tests.py
+++ tests/preview_tests/preview_android_tests.py
@@ -21,6 +21,9 @@ from core.settings.settings import TEST_RUN_HOME, EMULATOR_ID, EMULATOR_NAME, WE
 class PreviewCommandTestsAndroid(BaseClass):
     app_webpack_name = "TestAppWebpack"
     css_change_webpack = ['app/app.css', '18', '32']
+    EMULATOR_ID_SECOND = "emulator-5556"
+    EMULATOR_NAME_SECOND = "Emulator-Api24-Default"
+    EMULATOR_PORT_SECOND = "5556"
 
 
     @classmethod
@@ -257,9 +260,46 @@ class PreviewCommandTestsAndroid(BaseClass):
                    'Local plugin nativescript-geolocation differs in major version from plugin in preview app',
                    'Some features might not work as expected'
                   ]
-    def test_190_tns_preview_android_on_two_emulators(self):
-        Emulator.start("Emulator-Api24-Default", "5556")
-        Preview.install_preview_app("emulator-5556", platform=Platform.ANDROID)
+    
+    def test_190_tns_preview_android_check_only_one_emulator_refreshed(self):
+        """
+         When scanning QR code on multiple devices verify that only the current device is refreshed
+        """
+        # Start second emulator and install Preview on it
+        Emulator.start(self.EMULATOR_NAME_SECOND, self.EMULATOR_PORT_SECOND)
+        Preview.install_preview_app(self.EMULATOR_ID_SECOND, platform=Platform.ANDROID)
+
+        # `tns preview` and take the app url and open it in Preview App
+        log = Tns.preview(attributes={'--path': self.app_name}, wait=False,
+                        log_trace=True,assert_success=False)
+        strings = ['Use NativeScript Playground app and scan the QR code above to preview the application on your device']
+        Tns.wait_for_log(log_file=log, string_list=strings, timeout=180, check_interval=10, clean_log=False)
+        output = File.read(log)
+        url = Preview.get_url(output)
+        Preview.run_app(url, EMULATOR_ID, platform=Platform.ANDROID)
+                              
+        strings = ['Start syncing changes for platform android',
+                   'Successfully synced changes for platform android']
+        Tns.wait_for_log(log_file=log, string_list=strings, timeout=180, check_interval=10, clean_log=True)
+
+        # Tap on the button and verify the change in the app 
+        Device.click(device_id=EMULATOR_ID, text="TAP", timeout=30)
+        Device.screen_match(device_name=EMULATOR_NAME, device_id=EMULATOR_ID,
+                            expected_image='livesync-hello-world_preview_after_tap', timeout=15)
+        
+        # Run app in Preview on second emulator
+        Preview.run_app(url, self.EMULATOR_ID_SECOND, platform=Platform.ANDROID)
+        Tns.wait_for_log(log_file=log, string_list=strings, timeout=180, check_interval=10, clean_log=False)
+
+        # Verify app on first emulator in not reloaded
+        Device.screen_match(device_name=EMULATOR_NAME, device_id=EMULATOR_ID,
+                            expected_image='livesync-hello-world_preview_after_tap', timeout=15)
+
+
+    def test_195_tns_preview_android_livesync_on_two_emulators(self):
+        """ 
+         Scan QR code on two emulators and veify livesync on them
+        """ 
         # `tns preview` and take the app url and open it in Preview App
         log = Tns.preview(attributes={'--path': self.app_name}, wait=False,
                         log_trace=True,assert_success=False)
@@ -277,9 +317,25 @@ class PreviewCommandTestsAndroid(BaseClass):
         Device.screen_match(device_name=EMULATOR_NAME, device_id=EMULATOR_ID,
                             expected_image='livesync-hello-world_home_preview')
 
-        Preview.run_app(url, "emulator-5556", platform=Platform.ANDROID)
+        # Run app in Preview on second emulator
+        Preview.run_app(url, self.EMULATOR_ID_SECOND, platform=Platform.ANDROID)
         Tns.wait_for_log(log_file=log, string_list=strings, timeout=180, check_interval=10, clean_log=True)
         
         # Verify app looks correct inside emulator
         Device.screen_match(device_name="Emulator-Api24-Default", device_id="emulator-5556",
                             expected_image='livesync-hello-world_home_preview')
+
+        # Change JS file
+        ReplaceHelper.replace(self.app_name, ReplaceHelper.CHANGE_JS, sleep=10)
+        strings = ['Start syncing changes for platform android', 'Successfully synced main-view-model.js for platform android']
+        Tns.wait_for_log(log_file=log, string_list=strings)
+
+        # Verify app is synced on first emulator
+        text_changed = Device.wait_for_text(device_id=EMULATOR_ID, text='42 clicks left', timeout=30)
+        assert text_changed, 'Changes in JS file not applied (UI is not refreshed).'
+
+        # Verify app is synced on second emulator
+        text_changed = Device.wait_for_text(device_id=self.EMULATOR_ID_SECOND, text='42 clicks left', timeout=30)
+        assert text_changed, 'Changes in JS file not applied (UI is not refreshed).'
+
+    
